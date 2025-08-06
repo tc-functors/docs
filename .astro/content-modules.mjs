fb6c289a215aff45a5596a5104faa24adfb33e2a
icylisper <icylisper@gmail.com>
2025-08-05 21:22:21 -0700

    update doc

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fhello-world.mdx&astroContentModuleFlag=true")],
-["src/content/docs/examples/progress-tracker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fexamples%2Fprogress-tracker.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/examples/progress-tracker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fexamples%2Fprogress-tracker.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting-started/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
 		
