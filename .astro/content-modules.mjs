7552c57bb1efa9309a3ad4717c870c94c8717394
icylisper <icylisper@gmail.com>
2025-10-07 06:25:07 -0700

    update docs

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/examples/progress-tracker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fexamples%2Fprogress-tracker.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting-started/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/internals/compiler.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finternals%2Fcompiler.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/internals/composer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finternals%2Fcomposer.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/internals/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finternals%2Foverview.mdx&astroContentModuleFlag=true")],
--- src/content/docs/ui/inspector.md
+++ src/content/docs/ui/inspector.md
@@ -5,6 +5,10 @@ description: HTMX-based Inspector UI
 
 The `inspector` is a built-in HTMX-based UI to inspect core tc entities in the filesystem. It also has an interface to generate topologies using LLM models specific to tc.
 
+:::note
+The inspector is not meant to be a complete web interface to tc. It is a simple UI to display and visualize all the entities and topologies in the system.
+:::
+
 By default, `inspector` uses SurrealDB in-memory mode.
 
 
@@ -12,32 +16,6 @@ By default, `inspector` uses SurrealDB in-memory mode.
 tc inspect --port 8080
 
 => http://localhost:8080
-```
-
-### Optional external database
-
-Alternatively, we can run SurrealDB in network mode and connect to it.
-Follow instructions to install SurrealDB -  https://surrealdb.com/install
-
-
-```
-surreal start --user root --pass secret
-
-```
-Add following to tc-inspector.yml file.
-
-```yaml
-store_mode: Network
-address: 127.0.0.1:8000
-user: root
-password: secret
-```
-
-Run tc inspector with path to tc-inspector.yml file
-
-```
-cd tc/examples
-tc inspect --port 8080 --config tc-inspector.yml
 ```
 
 ### LLM
